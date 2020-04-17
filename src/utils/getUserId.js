99ec4eeddcd97fb0d621b280bd4dabe94279e21f
vattcarter7 <vattcarter@gmail.com>
2020-04-17 19:16:01 +0700

    fixed bug for jwt

--- src/utils/getUserId.js
+++ src/utils/getUserId.js
@@ -6,7 +6,7 @@ const getUserId = (request, requireAuth = true) => {
     : request.connection.context.Authorization;
   if (header) {
     const token = header.replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'thisisasecret');
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
     return decoded.userId;
   }
 
