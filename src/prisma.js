cc8dbf7b6301b2edd166d3ce5c62292db4c62de1
vattcarter7 <vattcarter@gmail.com>
2020-04-17 18:49:10 +0700

    extract prisma secret to env var

--- src/prisma.js
+++ src/prisma.js
@@ -3,7 +3,7 @@ import { fragmentReplacements } from './resolvers/index';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'thisismysupersecrettext',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 });
 
