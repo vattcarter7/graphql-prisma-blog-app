705c21701b0de788108f9faf7f0cce8fb65d88ba
vattcarter7 <vattcarter@gmail.com>
2020-04-17 18:59:27 +0700

    added jwt-secret env var in heroku

--- src/utils/generatedToken.js
+++ src/utils/generatedToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, 'thisisasecret', { expiresIn: '7 days' });
+  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30 days' });
 };
 
 export { generateToken as default };
